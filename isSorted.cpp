8a74e8ec59b30077cb697d60a3146d60fcf53b28
Ujjwal  Jha <72433046+Jhaujjwal4196@users.noreply.github.com>
2020-12-30 18:27:07 +0545

    Method@ for the same problem added

--- isSorted.cpp
+++ isSorted.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 using namespace std;
+bool isSorted2(int a[], int n){
+    if(n==0||n==1)
+    return true;
+    bool smallerOutput= isSorted2(a+1,n-1);
+    if (!smallerOutput)
+    return false;
+     if(a[0]>a[1])
+     return false;
+     else
+     {
+         return true;
+     }
+     
+}
 bool isSorted(int a[], int n){
     if(n==0||n==1)
     return true;
@@ -8,7 +22,6 @@ bool isSorted(int a[], int n){
     bool isSmallerSorted= isSorted(a+1, n-1);
     if(isSmallerSorted)
     return true;
-    
 }
 main(){
     cout<<"Please Provide us the length of array:\t";
@@ -23,6 +36,12 @@ cout<<"Yes the provided input aarray is sorted:"<<endl;
 else
 {
     cout<<"Sorry ..the provided array is unsorted:"<<endl;
+}
+if(isSorted2(arr,n))
+cout<<"Yes the provided input aarray is sorted:Checked through second method"<<endl;
+else
+{
+    cout<<"Sorry ..the provided array is unsorted:Checked through second method"<<endl;
 }
 
 
