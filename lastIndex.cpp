18e9c63121977f64f40273a455f28093ce18e1e2
Ujjwal  Jha <72433046+Jhaujjwal4196@users.noreply.github.com>
2020-12-31 14:37:19 +0545

    Bugs Fixed

--- lastIndex.cpp
+++ lastIndex.cpp
@@ -1,26 +1,21 @@
 #include <iostream>
 using namespace std;
 int lastIndex(int a[],int size, int value){
-    //cout<<a[size];
+   // cout<<a[size];
     if(a[size]==value)
     return size;
-    else if(size==0&&a[size]!=value)
+    if(size==0&&a[size]!=value)
     return 0;
     else
-    {
-        return lastIndex(a-1,size-1,value);
-    }
-    
-}
-main(){
-int a[5]={3,1,9,9,1};
-int b=5;
-int size=lastIndex(a,4,9);
-if(size)
-cout<<"Found First at position \t"<<size<<endl; 
-else
-{
-    cout<<"Not found"<<endl;
+    return lastIndex(a,size-1,value); 
 }
 
+main(){
+int a[5]={5,0,8,9,1};
+int b=5;
+int size=lastIndex(a,5,5);
+if(size>=0)
+cout<<"Starting from he end Found at position \t"<<size<<endl; 
+else
+cout<<"Not found"<<endl;
 }
