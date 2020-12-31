a58bf2f86139c7a1ffb3904134f8991c520fa937
Ujjwal  Jha <72433046+Jhaujjwal4196@users.noreply.github.com>
2020-12-31 09:30:37 +0545

    First Index of the provided value

---
+++ firstIndex.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int firstIndex(int a[], int size, int val){
+    
+if(a[0]==val)
+//cout<<"Found First at position \t"<<abs(size_Fix-size)<<endl; 
+return size;
+else if(size==0&&a[size]!=val)
+//cout<<"Not found"<<endl;
+return 0;
+else
+{
+    return firstIndex(a+1,size-1,val);
+}
+
+
+}
+main(){
+int a[5]={3,1,8,9,6};
+int b=5;
+int size=firstIndex(a,5,1);
+if(size)
+cout<<"Found First at position \t"<<abs(b-size)<<endl; 
+else
+{
+    cout<<"Not found"<<endl;
+}
+
+}
