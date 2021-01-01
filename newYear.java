4568368f22aadcfe3f3da969fee4d2dd0647fc2e
Ujjwal  Jha <72433046+Jhaujjwal4196@users.noreply.github.com>
2021-01-01 09:16:04 +0545

    NewYeaCodeInJava

---
+++ newYear.java
@@ -0,0 +1,19 @@
+/*********Happy New Year*******/
+import happinesss;
+import health;
+import wealth; 
+public class newYear{
+    public static void main(String[] args) {
+        time="begin 2021";
+        while(time<="end 2021"){
+            push(love);
+            push(joy);
+            push(happiness);
+            push(prosperity);
+            time++;
+        }
+        if(bad_memories)
+        pop(all);
+        System.out.println("Wishing You A very Happpy New Year");
+    }
+}
