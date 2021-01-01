1810f5f529b0d4eb55b6a2e5be11a4b96c1dacdb
Ujjwal  Jha <72433046+Jhaujjwal4196@users.noreply.github.com>
2021-01-01 09:15:24 +0545

    New Year Code Added

---
+++ newyr.cpp
@@ -0,0 +1,24 @@
+#include<prosperity>
+#include<health>
+#include<wealth>
+
+void push(string val){
+year[star_time++]="val";
+}
+
+void pop(){
+     if(bad_memories)
+     year[bad_memories]='\0';
+ }
+void newYear(start 2021){
+    whiile(2021){
+    push(joy);
+    push(success);
+    push(internship);
+    push(love);
+    pop();
+    cout<<"Wishing you a prosperous year ahead";}do newYear(time++);
+}
+main(){
+    newYear(start_2021);
+}
