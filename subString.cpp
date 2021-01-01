00ed5be1c73f8daa0ce6d3678bd785fc7fab2eb9
Ujjwal  Jha <72433046+Jhaujjwal4196@users.noreply.github.com>
2021-01-01 09:37:46 +0545

    Just Revised the same code

--- subString.cpp
+++ subString.cpp
@@ -4,11 +4,11 @@ int substring(string input , string output[]){
 if(input.empty()){
     output[0]="";
 return 1;}
-string smallstring = input.substr(1);
-int smalloutput=substring(smallstring,output);
-for(int i=0;i<smalloutput;i++)
-output[i+smalloutput]=input[0]+output[i];
-return 2*smalloutput;
+string smalloutput= input.substr(1);
+int smallSize= substring(smalloutput,output);
+for(int i=0;i<smallSize;i++){
+output[i+smallSize]= input[0]+output[i];}
+return 2*smallSize;
 }
 
 main(){
@@ -16,6 +16,7 @@ main(){
     string input;
     cout<<"please provide the input string for which the suset you're looking for:\t";
     cin>>input;
+    
     int size= substring(input,output);
     cout<<"Pleae find the substring for the given string"<<endl;
     for(int i=0;i<size;i++)
