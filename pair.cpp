ea2db09f5265f6e505f6444e74788f8c5575bc73
Ujjwal  Jha <72433046+Jhaujjwal4196@users.noreply.github.com>
2021-01-03 21:09:02 +0545

    Pair container class added

---
+++ pair.cpp
@@ -0,0 +1,15 @@
+#include <utility>;
+#include<iostream>
+using namespace std;
+main(){
+    pair <int ,int> p1(10,20);
+   // pair p3;
+  //  p3={3,40};
+  pair<int ,int>p3;
+ // p3=make_pair(10,100);
+p3={30,100};
+    pair<int ,string>p2(10,"UjjwalJha");
+cout<<p1.first<<"\t"<<p1.second<<endl;
+cout<<p2.first<<"\t"<<p2.second<<endl;
+
+}
