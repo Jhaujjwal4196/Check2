f5fdfd97e4b410e451ee37efa82dc06f87bc9dbb
Ujjwal  Jha <72433046+Jhaujjwal4196@users.noreply.github.com>
2021-01-04 13:04:49 +0545

    Commenting done fo the given prob

--- josephus.cpp
+++ josephus.cpp
@@ -1,21 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 int getSolved(int n, int k){
+    //create a doubly list and insert from 0 to n-1
     list<int> l;
     for(int i=0;i<n;i++){
         l.push_back(i);
     }
+    //point iterator at the startying of the list
     auto it= l.begin();
     while(l.size()>1){
         for(int i=1;i<k;i++){
             it++;
+            //point the iterator to the kth place and checlk if it is last point if it is the last one then make iterator point to the begininhg of it so that it can work like circular list;
             if(it==l.end())
               it=l.begin();
         }
+        //erase the kth point and check for the last one
       it=  l.erase(it);
           if(it==l.end())
               it=l.begin();
     }
+    //return the begining element
     return *l.begin();
 }
 main(){
