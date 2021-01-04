e9d83b8eb1f29beb8f76ebe67f43b55eb788a67a
Ujjwal  Jha <72433046+Jhaujjwal4196@users.noreply.github.com>
2021-01-04 10:55:58 +0545

    Some Corrections to be done

--- SotByMarks.cpp
+++ SotByMarks.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <vector>
 using namespace std;
 void SortByMarks(int a[],int b[],int n){
 pair <int, int>p1[n];
@@ -10,8 +11,18 @@ sort(p1,p1+n);
     cout<<p1[i].second<<"\t"<<p1[i].first<<"\n";
 
 }
+void SortByMarks(int a[],int b[], int n){
+    vector <pair<int,int>> v;
+    for(int i=0;i<n;i++)
+    v.push_back(b[i],a[i]);
+    sort(v.begin(),v.end(),greater<pair<int,int>>());
+    for(auto it=v.begin();it!=v.end();it++)
+   cout<<(*it).second<<"\t"<<"(*it).second"<<endl;
+
+}
 main(){
     int ar[]={105,106,109,108,102};
     int arr[]={70,69,36,89,11};
+    SortByMarks(ar,arr,5);
     SortByMarks(ar,arr,5);
 }
