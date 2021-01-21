1435854dfb00940fdda0245393c0807ee6a0c9d0
Ujjwal  Jha <72433046+Jhaujjwal4196@users.noreply.github.com>
2021-01-21 12:24:09 +0545

    q3 by robin useless

---
+++ q3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+main(){
+    int num;
+    printf("Enter any number: ");
+    scanf("%d", &num);
+    if(num & 1)
+        printf("%d is odd.", num);
+    else
+        printf("%d is even.", num);
+}
